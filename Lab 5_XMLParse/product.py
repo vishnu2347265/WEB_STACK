4b6d571290ce3370715aadb56b269378c99ca2b7
vishnu2347265 <vishnu.swaroop@mca.christuniversity.in>
2023-08-28 09:42:09 +0530

    path change

--- Lab 5_XMLParse/product.py
+++ Lab 5_XMLParse/product.py
@@ -1,24 +1,24 @@
 from lxml import etree
 
 # Load XML and XSL files
-xml = etree.parse("products.xml")
-xsl = etree.parse("transform.xsl")
-xsd = etree.parse("ecommerce.xsd")
+xml = etree.parse(r"Lab 5_XMLParse\products.xml")
+xsl = etree.parse(r"Lab 5_XMLParse\xsltransform.xsl")
+xsd = etree.parse(r"Lab 5_XMLParse\product_schema.xsd")
 
 # Apply XSL transformation
 transform = etree.XSLT(xsl)
 html = transform(xml)
-print(html)
+# print(html)
 
 # Save transformed HTML to a file
-with open('Labworks\Lab Ex5-XSL\ecommerceparse.html', 'wb') as output_file:
+with open('Lab 5_XMLParse\product.html', 'wb') as output_file:
     output_file.write(etree.tostring(html, pretty_print=True))
 
 # Load XSD schema
 schema = etree.XMLSchema(xsd)
 
 # Validate transformed HTML against XSD schema
-validation_result = schema.validate(html)
+validation_result = schema.validate(xml)
 
 if validation_result:
     print("Validation successful!")
