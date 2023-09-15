9bc7dfea691ecc87e3ebc8c7d7b63b34518b5111
vishnu2347265 <vishnu.swaroop@mca.christuniversity.in>
2023-09-15 15:57:09 +0530

    books

---
+++ Lab 7 AJAX_FETCH_JSON/script.js
@@ -0,0 +1,27 @@
+const fetchButton = document.getElementById("fetchBooks");
+const bookList = document.getElementById("booklist");
+var booklist=[];
+fetchButton.addEventListener("click", async ()=> {
+    const apiUrl = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=v8wv1c5u7u5NUATw7wxAPBHvIACOp2n5"; 
+
+	await fetch(apiUrl).then((res)=>
+	{
+		res.json().then((data)=>
+		{
+		
+			booklist=data.results.books;
+			console.log(data);
+			for(let i=0;i<booklist.length;i++)
+			{
+				var book=document.createElement('div');
+				book.className="bookdata";
+				book.innerHTML=  `Title: ${booklist[i].title}   Author: ${booklist[i].author}`;
+				document.getElementById("booklist").appendChild(book);
+			}
+		})
+	}).catch((err)=>
+	{
+		console.log(err);
+	})
+})
+
